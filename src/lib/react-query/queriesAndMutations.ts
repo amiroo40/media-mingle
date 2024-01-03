2ea40a44fd0e31e6e399017e16c3b7f7e246432b
amir <amirreza.s.1414@gmail.com>
2024-01-03 17:47:24 +0330

    12345

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,4 +1,4 @@
-import { INewUser } from '@/types';
+import { INewPost, INewUser } from '@/types';
 import {
     useQuery,
     useMutation,
@@ -6,6 +6,7 @@ import {
     useInfiniteQuery
 } from '@tanstack/react-query';
 import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { CreatePost } from '@/_root/pages';
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -27,3 +28,15 @@ export const useSingOutAccount = () => {
         mutationFn: signOutAccount
     })
 }
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFnnFn: (post: INewPost) => CreatePost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+            });
+        },
+    });
+};
